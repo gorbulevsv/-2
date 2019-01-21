6f475c1dd96563fd62c788c4087ae9f0943f79e0
Горбулёв Сергей <gorbulevsv@yandex.ru>
2019-01-21 10:36:22 +0300

    1

---
+++ Приложение2/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Приложение2.Models
+{
+    public class ApplicationDbContext:IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ Приложение2/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Приложение2.Models
+{
+    public class User:IdentityUser
+    {
+        public int Год { get; set; }
+    }
+}
